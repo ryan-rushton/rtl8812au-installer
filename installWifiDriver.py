989eaff85b85284fcd25df485afadc93db03eb8d
Ryan Rushton <ryan.rushton79@gmail.com>
2017-06-17 18:54:27 +1000

    Add files via upload

---
+++ installWifiDriver.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import os
+import getpass
+import platform
+import shutil
+import subprocess
+import sys
+import time
+import zipfile
+
+if getpass.getuser() != 'root':
+    print('This must be run as root')
+    sys.exit()
+
+if len(sys.argv) == 1 or sys.argv[1] not in ['install', 'reload']:
+    print('Use arguement \'reload\' to reload the driver and \'install\' to do a full install')
+    sys.exit()
+
+if sys.argv[1] == 'install':
+    if os.path.isdir(os.path.join(os.getcwd(), 'rtl8812au-master')):
+        shutil.rmtree('rtl8812au-master')
+
+    if not os.path.isfile('rtl8812au-master.zip'):
+        print('This must be run from the dir that contains the rtl8812au-master.zip file')
+        sys.exit()
+
+    os.mkdir('rtl8812au-master')
+
+    print('Extracting files')
+    z = zipfile.ZipFile(os.path.join(os.getcwd(), 'rtl8812au-master.zip'))
+    z.extractall(os.getcwd())
+    z.close()
+
+    os.chdir('rtl8812au-master')
+
+    print('Starting make')
+    subprocess.Popen('make').wait()
+    shutil.copy('8812au.ko', '/lib/modules/' + platform.release() + '/kernel/drivers/net/wireless/8812au.ko')
+    subprocess.Popen('depmod').wait()
+
+print('Inserting module')
+try:
+    subprocess.Popen(['rmmod', '8812au.ko']).wait()
+    subprocess.Popen(['insmod', '/lib/modules/' + platform.release() + '/kernel/drivers/net/wireless/8812au.ko']).wait()
+except:
+    subprocess.Popen(['insmod', '/lib/modules/' + platform.release() + '/kernel/drivers/net/wireless/8812au.ko']).wait()
+    subprocess.Popen(['rmmod', '8812au.ko']).wait()
+    subprocess.Popen(['insmod', '/lib/modules/' + platform.release() + '/kernel/drivers/net/wireless/8812au.ko']).wait()
